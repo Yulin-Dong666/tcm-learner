2bd6feec33acead4bf22e503311ea6fadc45511f
Yulin-Dong666 <emagical927@gmail.com>
2025-08-08 18:33:22 +0800

    Delete schemas directory

--- schemas/index.ts
+++
@@ -1,7 +0,0 @@
-import post from './post'
-import page from './page'
-import glossaryEntry from './glossaryEntry'
-import meridian from './meridian'
-import acupoint from './acupoint'
-
-export default [post, page, glossaryEntry, meridian, acupoint]
